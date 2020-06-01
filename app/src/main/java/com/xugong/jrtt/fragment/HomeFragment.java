f737769db72bced952fe1446b63f3d2347074542
QST_wengzhixiong <hadoop101@126.com>
2020-06-01 15:33:55 +0800

    指令器

--- app/src/main/java/com/xugong/jrtt/fragment/HomeFragment.java
+++ app/src/main/java/com/xugong/jrtt/fragment/HomeFragment.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -28,12 +29,17 @@ public class HomeFragment extends Fragment {
     //3,定义了适配器
     class MyPagerAdapter extends FragmentPagerAdapter{
         private List<Fragment> pages=new ArrayList<>();
+        private List<String> titles=new ArrayList<>();
         public MyPagerAdapter(FragmentManager fm) {
             super(fm);
             pages.add(new Page1Fragment());//0
             pages.add(new Page2Fragment());//1
             pages.add(new Page3Fragment());//2
             pages.add(new Page4Fragment());//3
+            titles.add("Page1Fragment");
+            titles.add("Page2Fragment");
+            titles.add("Page3Fragment");
+            titles.add("Page4Fragment");
         }
 
         @Override
@@ -44,6 +50,12 @@ public class HomeFragment extends Fragment {
         @Override
         public int getCount() {//返回页面的数量
             return pages.size();
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles.get(position);
         }
     }
     @Nullable
@@ -62,6 +74,9 @@ public class HomeFragment extends Fragment {
         //4,适配器
         MyPagerAdapter myPager=new MyPagerAdapter(getFragmentManager());
         viewPager.setAdapter(myPager);
+        //5,初始指示器
+        TabLayout tablayout = view.findViewById(R.id.tablayout);
+        tablayout.setupWithViewPager(viewPager);//->viewpager-->adapter->getPageTitle
         return view;
     }
 }
