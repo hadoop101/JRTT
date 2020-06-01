27f4e6d2d43b16261890ddf0ef5b30658dd9cf40
QST_wengzhixiong <hadoop101@126.com>
2020-06-01 14:02:14 +0800

    配置文件

---
+++ app/src/main/java/com/xugong/jrtt/fragment/UserFragment.java
@@ -0,0 +1,31 @@
+package com.xugong.jrtt.fragment;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class UserFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View view = getMyView();
+            return view;
+
+    }
+
+    private View getMyView() {
+        TextView textView= new TextView(getActivity());
+        textView.setBackgroundColor(Color.BLUE);
+        textView.setText("UserFragment");
+        textView.setTextSize(22);
+        textView.setGravity(Gravity.CENTER);
+        return textView;
+    }
+}
